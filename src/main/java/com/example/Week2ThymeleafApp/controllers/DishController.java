fe59c8fc2b698ae9b8a3217aa1855bfbf38d488e
sigehiro <sigehiro.0728@gmail.com>
2025-01-21 20:21:27 -0500

    Botstrap で簡単なLayoutとCSSを実装

--- src/main/java/com/example/Week2ThymeleafApp/controllers/DishController.java
+++ src/main/java/com/example/Week2ThymeleafApp/controllers/DishController.java
@@ -1,14 +1,20 @@
 package com.example.Week2ThymeleafApp.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DishController {
 
+    @Value("${restaurant.name}")
+    private String restaurantName;
+
     //endpoint for home page
     @GetMapping("/home")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("rName", restaurantName);
         return "home";
     }
 
